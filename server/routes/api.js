869570be42958fe28116ca59a0d0450dd4cdec21
gurukara7 <34938846+gurukara7@users.noreply.github.com>
2017-12-29 17:33:29 +0530

    Initial Code
    Initial Code

---
+++ server/routes/api.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// create a schema
+var recruitSchema = new Schema({
+    name: String,
+    number: Number,
+    joining: Boolean
+});
+
+mongoose.connect('mongodb://parrot:TiPxAn9NZktsUVlD@ds133127.mlab.com:33127/penguin', { useMongoClient: true });
+var recruits = mongoose.model('recruits', recruitSchema);
+
+
+// Get users
+router.get('/recruits', (req, res) => {
+    recruits.find({}, function (err, recruits) {
+        if (err) throw err;
+        res.json(recruits);
+    }).sort({ joining: 1 });
+});
+
+module.exports = router;
+
+
+
+
+
+
+
+
+//const MongoClient = require('mongodb').MongoClient;
+//const ObjectID = require('mongodb').ObjectID;
+
+// make this available to our users in our Node applications
+//module.exports = User;
+
+// Connect
+//const connection = (closure) => {
+//    return MongoClient.connect('mongodb://parrot:TiPxAn9NZktsUVlD@ds133127.mlab.com:33127/penguin', (err, db) => {
+//        if (err) return console.log(err);
+
+//        closure(db);
+//    });
+//};
+
+//// Error handling
+//const sendError = (err, res) => {
+//    response.status = 501;
+//    response.message = typeof err == 'object' ? err.message : err;
+//    res.status(501).json(response);
+//};
+
+// Response handling
+//let response = {
+//    status: 200,
+//    data: [],
+//    message: null
+//};
